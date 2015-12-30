fef66d6e8b0dfb06adca3f5d7bcf212cef3f514c
Martin Domke <mdomke@macbook-pro-82.local>
2015-12-30 14:38:00 +0100

    Added texclean and py-reload helper functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,3 +4,17 @@
 mkdf() {
   touch "$(date +'%Y-%m-%d')_$@"
 }
+
+
+# Cleanup latex helper files
+texclean() {
+  rm -f ./*.{aux,log}
+}
+
+
+# Re-install the current project into the active virtual environment
+py-reload() {
+  local project=$(basename $PWD)
+  pip uninstall -y ${project}
+  pip install .
+}
